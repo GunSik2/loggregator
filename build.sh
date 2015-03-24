0eee5ae6ed31507b7f4026f5ca79cdcb53b283d0
GunSik Choi <cgshome@gmail.com>
2015-03-24 05:18:47 +0000

    new one

--- build.sh
+++ build.sh
@@ -1,18 +1,5 @@
 #/bin/bash
 
+echo `pwd`
+
 # install Go tools
-export GOPATH=`pwd`
-go get golang.org/x/tools/cmd/vet
-go get golang.org/x/tools/cmd/cover
-#go get code.google.com/p/go.tools/cmd/vet
-#go get code.google.com/p/go.tools/cmd/cover
-
-go get -v github.com/mattn/goveralls
-go get -v github.com/onsi/ginkgo
-go get -v github.com/onsi/gomega
-go get -v github.com/apcera/nats
-go install -v github.com/onsi/ginkgo/ginkgo
-git submodule update --init --recursive
-
-# running tests
-bin/test
